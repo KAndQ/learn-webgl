8cad8798443181902de4cca779ba662fd79150c1
yaozhenyu <609045030@qq.com>
2022-02-14 21:12:34 +0800

    update.

---
+++ buildin.js
@@ -0,0 +1,34 @@
+this.createShader = function (gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+};
+
+this.createProgram = function (gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+};
+
+this.setRectangle = function (gl, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]), gl.STATIC_DRAW);
+};
